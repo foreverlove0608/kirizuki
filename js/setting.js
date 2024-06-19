f7aa5e9808065c7aed5efa1fc99c315090dd58d9
Hung <hung@m.ibis.studio>
2024-06-19 17:36:47 +0700

    up

--- js/setting.js
+++ js/setting.js
@@ -1,1 +1,20 @@
-"use strict";$(window).width()<835&&$(".js-customer").slick({infinite:!0,slidesToShow:3,slidesToScroll:1});
+"use strict"; $(window).width() < 835 && $(".js-customer").slick({ infinite: !0, slidesToShow: 3, slidesToScroll: 1 });
+
+
+var headerHeight = $('#main-header').outerHeight();
+var urlHash = location.hash;
+if (urlHash) {
+	$('body,html').stop().scrollTop(0);
+	setTimeout(function () {
+		var target = $(urlHash);
+		var position = target.offset().top - headerHeight;
+		$('body,html').stop().animate({scrollTop: position}, 1000);
+	}, 100);
+}
+
+$('.js-scroll').click(function () {
+	var href = $(this).attr("href");
+	var target = $(href);
+	var position = target.offset().top ;
+	$('body,html').stop().animate({scrollTop: position}, 1000);
+});
